668f2f7830dad83393ee61f36170b1a2b0e3ee32
alaricourge <alaricdebastard@gmail.com>
2025-03-24 10:51:01 +0100

    duplicate for sound policyNet

--- src/models.py
+++ src/models.py
@@ -266,9 +266,9 @@ class FullObsMinigridStateEmbeddingNet(nn.Module):
         return core_input
 
 
-class MinigridPolicyNet(nn.Module):
+class MinigridPolicyNet_Sound(nn.Module):
     def __init__(self, observation_dict, num_actions):
-        super(MinigridPolicyNet, self).__init__()
+        super(MinigridPolicyNet_Sound, self).__init__()
         self.observation_shape = observation_dict["image"].shape
         self.observation_shape_sound=observation_dict["sound"].shape
         self.num_actions = num_actions
@@ -336,6 +336,88 @@ class MinigridPolicyNet(nn.Module):
         x_tot = torch.cat((x, x_sound), dim=1) # concat 
 
         core_input = self.fc(x_tot)
+
+        core_input = core_input.view(T, B, -1)
+        core_output_list = []
+        notdone = (~inputs['done']).float()
+        for input, nd in zip(core_input.unbind(), notdone.unbind()):
+            nd = nd.view(1, -1, 1)
+            core_state = tuple(nd * s for s in core_state)
+            output, core_state = self.core(input.unsqueeze(0), core_state)
+            core_output_list.append(output)
+        core_output = torch.flatten(torch.cat(core_output_list), 0, 1)
+        
+        policy_logits = self.policy(core_output)
+        baseline = self.baseline(core_output)
+
+        if self.training:
+            action = torch.multinomial(
+                F.softmax(policy_logits, dim=1), num_samples=1)
+        else:
+            action = torch.argmax(policy_logits, dim=1)
+
+        policy_logits = policy_logits.view(T, B, self.num_actions)
+        baseline = baseline.view(T, B)
+        action = action.view(T, B)
+
+        return dict(policy_logits=policy_logits, baseline=baseline, 
+                    action=action), core_state
+
+class MinigridPolicyNet(nn.Module):
+    def __init__(self, observation_dict, num_actions):
+        super(MinigridPolicyNet, self).__init__()
+        self.observation_shape = observation_dict["image"].shape
+        self.num_actions = num_actions
+
+        init_ = lambda m: init(m, nn.init.orthogonal_, 
+            lambda x: nn.init.constant_(x, 0), 
+            nn.init.calculate_gain('relu'))
+        
+        self.feat_extract = nn.Sequential(
+            init_(nn.Conv2d(in_channels=self.observation_shape[2], out_channels=32, kernel_size=(3, 3), stride=2, padding=1)),
+            nn.ELU(),
+            init_(nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), stride=2, padding=1)),
+            nn.ELU(),
+            init_(nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), stride=2, padding=1)),
+            nn.ELU(),
+        )
+    
+        self.fc = nn.Sequential(
+            init_(nn.Linear(32, 1024)),
+            nn.ReLU(),
+            init_(nn.Linear(1024, 1024)),
+            nn.ReLU(),
+        )
+
+        self.core = nn.LSTM(1024, 1024, 2)
+
+        init_ = lambda m: init(m, nn.init.orthogonal_, 
+            lambda x: nn.init.constant_(x, 0))
+
+        self.policy = init_(nn.Linear(1024, self.num_actions))
+        self.baseline = init_(nn.Linear(1024, 1))
+
+
+    def initial_state(self, batch_size):
+        return tuple(torch.zeros(self.core.num_layers, batch_size, 
+                                self.core.hidden_size) for _ in range(2))
+
+
+    def forward(self, inputs, core_state=()):
+        # -- [unroll_length x batch_size x height x width x channels]
+        x = inputs['partial_obs']
+        T, B, *_ = x.shape
+
+        # -- [unroll_length*batch_size x height x width x channels]
+        x = torch.flatten(x, 0, 1)  # Merge time and batch.
+
+        x = x.float() #/ 255.0
+        
+        # -- [unroll_length*batch_size x channels x width x height]
+        x = x.transpose(1, 3)
+        x = self.feat_extract(x)
+        x = x.view(T * B, -1)
+        core_input = self.fc(x)
 
         core_input = core_input.view(T, B, -1)
         core_output_list = []
